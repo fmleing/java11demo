e9e5adf35a6b9c9bd21b2819032551b3b859ce22
Mengleifeng <fmleing@163.com>
2020-05-08 22:36:12 +0800

    StripLeading 用例

--- src/main/java/com/feng/java11/demo/StringDemo.java
+++ src/main/java/com/feng/java11/demo/StringDemo.java
@@ -3,7 +3,11 @@ package com.feng.java11.demo;
 public class StringDemo {
 
     public static void main(String[] args) {
-        String willStrip = "  aaa";
+        String willStrip = "  Strip  ";
         System.out.println(willStrip.strip());
+        String willStripLeading = " StripLeading  ";
+        System.out.println(willStripLeading.stripLeading());
+        String willStripTrailing = " StripTrailing  ";
+        System.out.println(willStripTrailing.stripTrailing());
     }
 }
