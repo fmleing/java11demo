e9468784dff5782ba235d9df70d53f5b2729c2b3
Mengleifeng <fmleing@163.com>
2020-05-08 22:32:11 +0800

    trip用例

---
+++ src/main/java/com/feng/java11/demo/StringDemo.java
@@ -0,0 +1,9 @@
+package com.feng.java11.demo;
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+        String willStrip = "  aaa";
+        System.out.println(willStrip.strip());
+    }
+}
